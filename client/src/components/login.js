67e665acbe16dc29b35d64053c68106c9aa18aae
Jack Zheng <jackzheng10@yahoo.com>
2020-03-31 03:26:00 -0400

    initial implementation test

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -12,6 +12,7 @@ import Container from "@material-ui/core/Container";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import loginStyles from "../styles/loginStyles";
+import ParticlesBg from "particles-bg";
 
 function Copyright() {
   return (
@@ -34,76 +35,96 @@ class Login extends Component {
   render() {
     const classes = this.props.classes;
 
+    let config = {
+      num: [4, 7],
+      rps: 0.1,
+      radius: [5, 40],
+      life: [1.5, 3],
+      v: [2, 3],
+      tha: [-50, 50],
+      alpha: [0.6, 0],
+      scale: [0.1, 0.9],
+      body:
+        "https://cdn0.iconfinder.com/data/icons/cleaning-52/48/Cleaning_expand-05-512.png",
+      position: "all",
+      //color: ["random", "#ff0000"],
+      cross: "dead",
+      random: 10
+    };
+
     return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Container className={classes.center}>
-            <img
-              style={{
-                width: 500,
-                height: 200
-              }}
-              src="https://www.laundr.io/wp-content/uploads/2020/03/user_img.png"
-            ></img>
-          </Container>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
+      <div>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Container className={classes.center}>
+              <img
+                style={{
+                  width: 500,
+                  height: 200
+                }}
+                src="https://www.laundr.io/wp-content/uploads/2020/03/user_img.png"
+              ></img>
+            </Container>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form className={classes.form}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/register" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
-      </Container>
+            </form>
+          </div>
+          <Box mt={8}>
+            <Copyright />
+          </Box>
+        </Container>
+        <ParticlesBg type="custom" config={config} bg={true} />
+      </div>
     );
   }
 }
