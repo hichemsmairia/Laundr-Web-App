e5a6cf47caa2e92c25419982f27a967693ee9f31
Jack Zheng <jackzheng10@yahoo.com>
2020-04-05 01:16:05 -0400

    Testing some integration with template

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import Login from "./components/login";
-import Register from "./components/register";
-import UserDashboard from "./components/userDashboard";
+import Login from "./views/login";
+import Register from "./views/register";
+import UserDashboard from "./views/userDashboard";
+
+import { Main as MainLayout } from "./components/layouts";
+import RouteWithLayout from "./components/RouteWithLayout";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "./theme";
 
 //todo: make page not found
 
@@ -14,6 +19,14 @@ export default class App extends Component {
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/userDashboard" component={UserDashboard} />
+          <ThemeProvider theme={theme}>
+            <RouteWithLayout
+              component={UserDashboard}
+              exact
+              layout={MainLayout}
+              path="/dashboard"
+            />
+          </ThemeProvider>
           <Route exact path="/">
             <Redirect to="/login" />
           </Route>
