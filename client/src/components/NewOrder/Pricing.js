c45b5a2f1bf034230645527a60a5b4ea0247a42f
Jack Zheng <jackzheng10@yahoo.com>
2020-04-17 16:45:05 -0400

    Fixing case-sensitive file names

--- client/src/components/NewOrder/Pricing.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Pricing extends Component {
-  render() {
-    return <h1>Pricing</h1>;
-  }
-}
-
-export default Pricing;
--- client/src/components/UserDashboard.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class UserDashboard extends Component {
-  render() {
-    return <h1>ayy wassup</h1>;
-  }
-}
-
-export default UserDashboard;
